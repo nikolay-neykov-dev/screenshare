b80fc4d1e76d0a451fb7dc44b60d3715792ad7de
Nikolay Neykov <nikolay.neykov@proton.me>
2023-08-12 02:22:32 +0300

    wp

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { SocketIoModule, SocketIoConfig } from "ngx-socket-io";
+import { SocketIoModule } from "ngx-socket-io";
 import { AppComponent } from "./app.component";
-
-const config: SocketIoConfig = { url: "http://localhost:3000", options: {} };
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, SocketIoModule.forRoot(config)],
+    imports: [BrowserModule, SocketIoModule.forRoot({ url: window.location.href })],
     providers: [],
     bootstrap: [AppComponent],
 })
